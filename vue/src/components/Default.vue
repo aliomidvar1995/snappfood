7e19a7197e39c256b364b0e2f1da0048d351cd3d
Ali Omidvar <aliomidvar14011995@gmail.com>
2023-06-01 00:04:02 +0330

    final change

--- vue/src/components/Default.vue
+++ vue/src/components/Default.vue
@@ -13,9 +13,6 @@
                 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div class="flex h-16 items-center justify-between">
                         <div class="flex items-center">
-                            <!-- <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-            </div> -->
                             <div class="hidden md:block">
                                 <div
                                     class="ml-10 flex items-baseline space-x-4 gap-3"
@@ -40,10 +37,6 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-4 flex items-center md:ml-6">
-                                <!-- <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="h-6 w-6" aria-hidden="true" />
-              </button> -->
 
                                 <!-- Profile dropdown -->
                                 <Menu as="div" class="relative ml-[100px]">
@@ -211,24 +204,8 @@
 </template>
 
 <script setup>
-import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import axiosClient from "../axios";
-
-const router = useRouter();
-// const store = useStore();
-// const user = store.state.user;
-
-function logout() {
-    axiosClient.post("/logout").then(({ data }) => {
-        console.log(data);
-        localStorage.removeItem("user");
-        localStorage.removeItem("token");
-        router.push({ name: "Login" });
-        return data;
-    });
-}
-
 import {
     Disclosure,
     DisclosureButton,
@@ -240,17 +217,23 @@ import {
 } from "@headlessui/vue";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
 
-const user = {
-    name: "Tom Cook",
-    email: "tom@example.com",
-    imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-const navigation = [{ name: "رستوران ها", href: "#", current: true }];
+const router = useRouter();
+
+function logout() {
+    axiosClient.post("/logout").then(({ data }) => {
+        console.log(data);
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+        router.push({ name: "Login" });
+        return data;
+    });
+}
+
 const userNavigation = [
     { name: "پروفایل", href: {name: 'Profile'} },
     { name: "ویرایش پروفایل", href: {name: 'EditProfile'} },
     { name: "تعیین آدرس", href: {name: 'CreateAddress'} },
     { name: "آدرس ها", href: {name: 'Addresses'} }
 ];
+
 </script>
