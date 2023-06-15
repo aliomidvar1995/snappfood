836140b0534fbcafc9e94fa684895226e0df1b97
Ali Omidvar <aliomidvar14011995@gmail.com>
2023-06-15 17:16:22 +0330

    update final version

--- resources/views/admin/comment/index.blade.php
+++ resources/views/admin/comment/index.blade.php
@@ -26,15 +26,7 @@
                         <input type="hidden" name="comment_id" value="{{ $comment->id }}">
                         <button class="btn btn-outline-danger" type="submit">حذف نظر</button>
                     </form>
-                    @if (!$comment->reply)
-                        <form class="mt-3" action="{{ route('seller.replies.store', ['restaurant' => $restaurant, 'comment' => $comment]) }}" method="post">
-                            @csrf
-                            <div class="form-group">
-                                <textarea placeholder="پاسخ شما" class="form-control" name="content" rows="5"></textarea>
-                                <button class="btn btn-outline-secondary mt-1" type="submit">ارسال</button>
-                            </div>
-                        </form>
-                    @else
+                    @if(!empty($comment->reply->content))
                         <p class="p-1 rounded me-3" style="background-color: #ccc">{{ $comment->reply->content }}</p>
                     @endif
                 </div>
